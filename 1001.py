12ea7cb96a541d3d73bdd1bdf0fbcb5247f87360
dudumilan <carloseduardomilann@gmail.com>
2025-04-10 20:24:55 -0300

    .

---
+++ 1001.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+A = int(input())
+B = int(input())
+X = A + B
+print(f"X = {X}")
---
+++ 1002.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+pi= 3.14159
+raio = float(input())
+area = pi * (raio  ** 2)
+print(f"A={area:.4f}")
